43535f8ef9b84f17e0db39e34e8cbbec6bb14d25
Phil Brockwell <nineteen40eight@gmail.com>
2018-07-23 16:42:45 +0100

    Add Google Analytics

---
+++ js/google_analytics.js
@@ -0,0 +1,14 @@
+var tracking_code = "UA-122787245-1";
+
+var script = document.createElement("script");
+script.src = `https://www.googletagmanager.com/gtag/js?id=${tracking_code}`;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", tracking_code);
