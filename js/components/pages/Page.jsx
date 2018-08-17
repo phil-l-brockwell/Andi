569cbdb06deec4b50a50c6f8d147dfc1b63c881f
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-17 13:24:36 +0100

    Scroll Page to top when updated

--- js/components/pages/Page.jsx
+++ js/components/pages/Page.jsx
@@ -1,20 +1,36 @@
 class Page extends React.Component {
+  constructor() {
+    super();
+    this.nextPage = this.nextPage.bind(this);
+    this.backPage = this.backPage.bind(this);
+  }
+
   render() {
     return (
-      <div className="display">
+      <div id="display" className="display">
         <Heading text={this.props.name + " details"} />
 
         {this.props.children}
 
         <Navigation>
-          <Button onClick={this.props.backPage} text="back" />
+          <Button onClick={this.backPage} text="back" />
           <Button
-            onClick={this.props.nextPage}
+            onClick={this.nextPage}
             disabled={!this.props.isComplete()}
             text="next"
           />
         </Navigation>
       </div>
     );
+  }
+
+  nextPage() {
+    document.getElementById('display').scrollTo(0,0);
+    this.props.nextPage();
+  }
+
+  backPage() {
+    document.getElementById('display').scrollTo(0,0);
+    this.props.backPage();
   }
 }
